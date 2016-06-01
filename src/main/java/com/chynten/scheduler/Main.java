226c58ec1facfbabfa20055ba6b57f7c74a9f66e
Chintan Patel <chintanpatel.it@gmail.com>
2016-06-01 17:28:17 +0530

    Initial push

---
+++ src/main/java/com/chynten/scheduler/Main.java
@@ -0,0 +1,14 @@
+package com.chynten.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chintankumar.patel on 6/1/2016.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
